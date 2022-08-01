76d7312a3dd743015a6369e79064dd124329e62e
Harsh Mukhiya <ha981muk@gmail.com>
2022-08-02 00:38:18 +0200

    u01 Software Design

---
+++ u01DesignPattern/src/WorkWithAnimals.java
@@ -0,0 +1,26 @@
+public class WorkWithAnimals {
+    public static void main(String[] args){
+        Dog fido = new Dog();
+
+        fido.setName("Fido");
+        System.out.println(fido.getName());
+        fido.digHole();
+        fido.setWeight(-1);
+
+
+        int randNum = 10;
+
+        fido.changeVar(randNum);                                    // randNum in method is 12
+
+        System.out.println("randNum after method call: "+ randNum); // randNum after method is 10
+
+        changeObjectName(fido);                                     // static method is called without object name
+
+        System.out.println("Dog name after method call: "+ fido.getName());
+
+    }
+
+    public static void changeObjectName(Dog fido){
+        fido.setSound("Marcus");
+    }
+}
